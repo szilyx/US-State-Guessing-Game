375738b5c16e7054e47c80b2aeb7e24fbf0c402e
szilyx <forgacsszilard1@gmail.com>
2025-05-17 15:34:18 +0300

    Final project

--- main.py
+++ main.py
@@ -14,10 +14,19 @@ while len(guessed_states) < 50:
     def getmouselocation(x,y):
         print(x, y)
 
-    answer_state = screen.textinput(title=f"Guess the State. {len(guessed_states)} already guessed", prompt="Whats the name of the state?")
+    answer_state = screen.textinput(title=f"Guess the State. {len(guessed_states)} already guessed", prompt="Whats the name of the state?").title()
 
     states = data.state.to_list()
     screen.onscreenclick(getmouselocation)
+    if answer_state.title() == "Exit":
+        need_to_learn = []
+        for i in states:
+            if i not in guessed_states:
+                need_to_learn.append(i)
+        df = pd.DataFrame(need_to_learn)
+        df.to_csv("States to learn")
+        break
+
     if answer_state in states:
         t = turtle.Turtle()
         t.hideturtle()
@@ -27,7 +36,3 @@ while len(guessed_states) < 50:
         t.write(answer_state)
         guessed_states.append(answer_state)
 
-
-
-
-turtle.mainloop()
