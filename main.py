9c642e6840419a8d280e2d0aa6df415f1f4bddde
szilyx <forgacsszilard1@gmail.com>
2025-05-17 15:18:48 +0300

    Guess function added

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import turtle
 from multiprocessing.connection import answer_challenge
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+data = pd.read_csv("50_states.csv")
+print(data)
+guessed_states = []
+while len(guessed_states) < 50:
+    def getmouselocation(x,y):
+        print(x, y)
 
-def getmouselocation(x,y):
-    print(x, y)
+    answer_state = screen.textinput(title=f"Guess the State. {len(guessed_states)} already guessed", prompt="Whats the name of the state?")
 
-screen.onscreenclick(getmouselocation)
+    states = data.state.to_list()
+    screen.onscreenclick(getmouselocation)
+    if answer_state in states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        data_loc = data[data.state == answer_state]
+        t.goto(data_loc.x.item(),data_loc.y.item())
+        t.write(answer_state)
+        guessed_states.append(answer_state)
 
-answer_state = screen.textinput(title="Guess the State", prompt = "Whats the name of the state?")
+
+
+
 turtle.mainloop()
